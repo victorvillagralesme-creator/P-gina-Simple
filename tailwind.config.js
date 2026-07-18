2f593b3abaa490817995e6f6f57a1f41b3a6401a
Victor Villagra <victorvillagra.lesme@gmail.com>
2026-07-17 22:05:18 -0300

    Se agrega el proyecto

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <script src="https://cdn.tailwindcss.com"></script>
+  <script>
+    tailwind.config = {
+      theme: {
+        extend: {
+          colors: {
+            'mi-color': '#123456'
+          }
+        }
+      }
+    }
+  </script>
+</head>
+<body>
+  <h1 class="text-mi-color">Hola</h1>
+</body>
+</html>
